bfe3c1bfeace78bf79e4080c2f1b286168542caf
Tim <timothyw.harrison@gmail.com>
2018-11-05 11:46:52 -0500

    WIP

--- server.js
+++ server.js
@@ -1,36 +1,50 @@
 const express = require("express");
 const app = express();
 const pug = require('pug');
+const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
 
 // Compile the source code
-const compiledFunction = pug.compileFile('views/index.pug');
-
+const mainView = pug.compileFile('views/index.pug');
+const portalLoginView = pug.compileFile('views/academic_login.pug');
 // app.set('view engine', 'pug')
 
-app.use(express.static(__dirname + '/public'));
+app.basedir = __dirname + '/views';
 
+// SCSS Middleware
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'scss'),
+  dest: path.join(__dirname, '/public/css'),
+  debug: true,
+  outputStyle: 'compressed',
+  force: true,
+  // prefix: '/css'
+}));
+app.use(express.static(path.join(__dirname, '/public')));
+
+// app.use('/images', express.static(path.join('.', 'public/images')));
+// app.use('/css', express.static(path.join('.', 'public/css')));
+// app.use('/scripts', express.static(path.join('.', 'public/js')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 
 // ROUTES
-app.get("/", function(req, res) {
-  // res.send("Hello World!");
-  // Render a set of datar
-
-  let org = req.params.organization; 
-
-  let html = compiledFunction(
-    {
-      name:
-        Math.random()
-    }
-  );
-
+app.get("/", (req, res) =>{
+  let html = mainView();
   // res.render("index");
   res.send(html);
 });
 
-
-app.get("/login", (req, res) => {
-  res.render('login');
+app.get('/:organization/login/', (req, res) => {
+  let org = req.params.organization;
+  let html = portalLoginView(
+    {
+      basedir: __dirname + '/views',
+      name: org,
+      brandColorPrimary: '#f00',
+      brandColorSecondary: '#0f0'
+    }
+  )
+  res.send(html);
 })
 
 app.listen(3000, function() {
